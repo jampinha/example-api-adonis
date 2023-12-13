0e276a0696b35058a02d583833688557cbaff9d9
Mario Bandeira <josemariobandeira@hotmail.com>
2023-12-13 14:36:56 -0300

    feat: protect API routes with authentication rules

--- app/Controllers/Http/CategoriesController.ts
+++ app/Controllers/Http/CategoriesController.ts
@@ -2,12 +2,16 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Category from 'App/Models/Category'
 
 export default class CategoriesController {
-  public async index({ response }: HttpContextContract) {
+  public async index({ auth, response }: HttpContextContract) {
+    await auth.use('api').authenticate()
+
     const data = await Category.all()
     return response.ok(data)
   }
 
-  public async show({ request, response }: HttpContextContract) {
+  public async show({ auth, request, response }: HttpContextContract) {
+    await auth.use('api').authenticate()
+
     const id = request.param('id')
     const category = await Category.find(id)
 
